4c28ce442965ced2845c7e6e7b4e71ec79eb4ef8
samysrini05 <65869364+samysrini05@users.noreply.github.com>
2021-12-22 14:18:39 -0500

    Update main.cpp

--- EarlySeason/src/main.cpp
+++ EarlySeason/src/main.cpp
@@ -1,16 +1,28 @@
 #include "main.h"
 #include "config.h"
 using namespace okapi;
-
+int i;
 void initialize() {
-  intake.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
+  front_intake.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
+  back_intake.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
   lift.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
-  intake.tarePosition();
+  front_intake.tarePosition();
+  back_intake.tarePosition();
   lift.tarePosition();
+
+  i = auton_selector();
+  gyro.reset();
 }
 
 void autonomous() {
-  auton();
+  switch(i){
+    case 0: mbl_goal();
+    break;
+    case 1: left_win_point();
+    break;
+    case 2: right_win_point();
+    break;
+  }
   stop();
 }
 
@@ -20,43 +32,36 @@ void opcontrol() {
       joystick.getAnalog(ControllerAnalog::leftY),
       joystick.getAnalog(ControllerAnalog::rightX)
     );
-    
-    if(takeIn.isPressed()){
-    frontIntakeClampDown();
-    }
-    else if(takeOut.isPressed()){
-      frontIntakeUp();
-    }
-    else{
-      intake.moveVoltage(0);
-    }
 
-    if(liftLimitSwitch.isPressed()){
-      lift.moveVelocity(0);
+    if(frontTakeIn.isPressed()){
+      front_clamp();
     }
-    //else if(pot.controllerGet() > a certain value){
-    //  lift.moveVelocity(0);
-    //}
-    else{
-      if(liftUp.isPressed()){
-        liftMoveUp();
-      }
-      else if(liftDown.isPressed()){
-        liftMoveDown();
-      }
-      else{
-        lift.moveVelocity(0);
-      }
-    }
-
-    if(clawOpen.isPressed()){
-      backClawOpen();
-    }
-    else if(clawClose.isPressed()){
-      backClawClose();
+    else if(frontTakeOut.isPressed()){
+      front_unclamp();
     }
     else{
+      front_intake.moveVoltage(0);
       delay(10);
+    }
+    
+    if(backTakeIn.isPressed()){
+      back_clamp();
+    }
+    else if(backTakeOut.isPressed()){
+      back_unclamp();
+    }
+    else{
+      back_intake.moveVoltage(0);
+    }
+
+    if(liftUp.isPressed()){
+      lift_up();
+    }
+    else if(liftDown.isPressed()){
+      lift_down();
+    }
+    else{
+      lift.moveVelocity(0);
     }
 
     delay(10);
