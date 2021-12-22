57255a912c4611933283fa5dde5e191176b55178
samysrini05 <65869364+samysrini05@users.noreply.github.com>
2021-12-22 14:18:19 -0500

    Update auton.cpp

--- EarlySeason/src/auton.cpp
+++ EarlySeason/src/auton.cpp
@@ -1,9 +1,122 @@
 #include "main.h"
 #include "config.h"
 using namespace okapi;
+
+void mbl_goal(){
+  chassis->setMaxVelocity(150);
+  int count = 0;
+  while(!back_bumperswitch.isPressed() && count < 80){
+    move_dist(150, 25);
+    count += 25;
+  }
+  stop();
+  for(int i = 0; i < 40; i++)
+  back_clamp();
+  stop();
+  chassis->setMaxVelocity(-150);
+  if(15000-(2*count) > 0){
+    move_dist(-150, 15000-(2*count));
+    stop();
+  }
+  stop();
+  chassis->setMaxVelocity(200);
+  stop();
+}
+
+void right_win_point(){
+  chassis->setMaxVelocity(40);
+  int count = 0;
+  while(!back_bumperswitch.isPressed() && count < 80){
+    move_dist(40, 25);
+    count += 25;
+  }
+  stop();
+  for(int i = 0; i < 40; i++)
+  front_clamp();
+  stop();
+  chassis->setMaxVelocity(-40);
+  if(15000-(2*count) > 0){
+    move_dist(-40, 15000-(2*count));
+    stop();
+  }
+  else{
+    move_dist(-40, 15000-(2*count)*-1);
+    stop();
+  }
+  chassis->setMaxVelocity(200);
+  stop();
+}
+
+void left_win_point(){
+  chassis->setMaxVelocity(40);
+  float count = 0;
+  while(!back_bumperswitch.isPressed()){
+    move_dist(40, 25);
+    count += 25;
+  }
+  stop();
+  for(int i = 0; i < 40; i++)
+  back_clamp();
+  stop();
+  back_unclamp();
+  stop();
+  chassis->setMaxVelocity(-60);
+  float time = 60.0/(40.0*count);
+  if(15000-(count + time) > 0){
+    move_dist(-60, 15000-(count + time));
+    stop();
+  }
+  chassis->setMaxVelocity(200);
+  stop();
+}
+
+void new_auton_trial(){
+  float count = 0;
+  chassis->setMaxVelocity(180);
+  while(!back_bumperswitch.isPressed()){
+    move_dist(180, 25);
+    count += 25;
+  }
+  move_dist(-180, count/2);
+  turn_cw(180, 25);
+  count = 0;
+  while(!back_bumperswitch.isPressed()){
+    move_dist(180, 25);
+    count += 25;
+  }
+  move_dist(-180, count/2);
+}
+/*
+go straight;
+grab neutral mogol(yellow one) with front intake (front is now the side with the lift );
+back up x amount idk just put some random num lol;
+turn 90 degrees counterclockwise (so that the back of our bot faces the alliance mogol);
+drive backward a lil;
+put back clamp down;
+move forward a lil to clear alliance mogol from win point line
+*/
+int auton_selector(){
+  printf("X - mbl, A - LWP, Y - RWP, B - None");
+  okapi::ControllerButton x(ControllerDigital::X);
+  okapi::ControllerButton a(ControllerDigital::A);
+  okapi::ControllerButton y(ControllerDigital::Y);
+  okapi::ControllerButton b(ControllerDigital::B);
+  if(x.isPressed()){
+    return 0;
+  }
+  else if(a.isPressed()){
+    return 1;
+  }
+  else if(y.isPressed()){
+    return 2;
+  }
+  else{
+    return -1;
+  }
+}
+
+/*
 void auton(){
-  turn_ccw(490);
-  /*
   while((int)ultraLeft.controllerGet() != (int)ultraRight.controllerGet()){
     if((int)ultraLeft.controllerGet() > (int)ultraRight.controllerGet()){
       fR.moveVelocity(5);
@@ -16,8 +129,7 @@ void auton(){
       delay(25);
     }
   }
-  */
-  /*
+
   while((int)ultraLeft.controllerGet() != (int)ultraRight.controllerGet()){
     if((int)ultraLeft.controllerGet() > (int)ultraRight.controllerGet()){
       fR.moveVelocity(5);
@@ -30,8 +142,7 @@ void auton(){
       delay(25);
     }
   }
-  */
-  /*
+
     chassis->setMaxVelocity(60);
     float count = 0;
     while(!intakeLimitSwitch.isPressed()){
@@ -65,102 +176,5 @@ void auton(){
     stop();
 
     chassis->setMaxVelocity(150);
-    */
-
-    chassis->setMaxVelocity(60);
-    float count = 0;
-    while(!intakeLimitSwitch.isPressed()){
-      moveDistance(60, 25);
-      count += 25;
-    }
-    stop();
-    for(int i = 0; i < 10; i++)
-    frontIntakeClampDown();
-    stop();
-    chassis->setMaxVelocity(-60);
-    moveDistance(-60, count*(float)3/4);
-    stop();
-    frontIntakeUp();
-    stop();
-    moveDistance(-60, count*(float)1/4);
-    stop();
-    chassis->setMaxVelocity(150);
-
-}
-void auton_selector(){
-
-}
-void moveDistance(float spd, int time){
-  fL.moveVelocity(spd);
-  fR.moveVelocity(spd);
-  bL.moveVelocity(spd);
-  bR.moveVelocity(spd);
-  delay(time);
-}
-void frontIntakeUp(){
-  intake.moveVoltage(8000);
-  delay(75); //experiment with this value
-}
-void frontIntakeClampDown(){
-  intake.moveVoltage(-8000);
-  delay(75); //experiment with this value
-}
-void backClawOpen(){
-  delay(1000);
-}
-void backClawClose(){
-  delay(1000);
-}
-void liftMoveUp(){
-  lift.moveVelocity(100);
-  delay(30); //experiment with this value
-}
-void liftMoveDown(){
-  lift.moveVelocity(-100);
-  delay(30); //experiment with this value
-}
-void stop(){
-  fL.moveVelocity(0);
-  fR.moveVelocity(0);
-  bL.moveVelocity(0);
-  bR.moveVelocity(0);
-  lift.moveVelocity(0);
-  intake.moveVoltage(0);
-}
-
-void turn_cw(int time){
-  fL.moveVelocity(100);
-  fR.moveVelocity(0);
-  bL.moveVelocity(100);
-  bR.moveVelocity(100);
-  delay(time);
-  stop();
-}
-void turn_ccw(int time){
-  fL.moveVelocity(0);
-  fR.moveVelocity(150);
-  bL.moveVelocity(0);
-  bR.moveVelocity(150);
-  delay(time);
-  stop();
-}/*
-void motionPID(float dist){
-  float error, integral, prevError, derivative = 0;
-  float power = 100;
-  while((int)pot.controllerGet() != (int)dist){
-    error = dist - pot.controllerGet();
-    integral += error;
-    if(error == 0 || (int)pot.controllerGet() == (int)dist){
-      integral = 0;
-    }
-    if(pot.controllerGet() > 10){ //if error is outside of useful range
-      integral = 0;
-    }
-    derivative = integral - prevError;
-    prevError = error;
-    power = error*kP + integral*kI + derivative*kD;
-    delay(15);
-    moveDistance(power, 15);
-  }
 }
 */
