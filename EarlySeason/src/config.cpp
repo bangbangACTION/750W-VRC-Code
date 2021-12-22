60546b5312fb8e99ee557ef2649dc10ad0d2a129
samysrini05 <65869364+samysrini05@users.noreply.github.com>
2021-12-22 14:17:50 -0500

    Update config.cpp

--- EarlySeason/src/config.cpp
+++ EarlySeason/src/config.cpp
@@ -1,47 +1,36 @@
 #include "main.h"
 #include "config.h"
-#define FRONT_LEFT_MOTOR 4
-#define FRONT_RIGHT_MOTOR 7
-#define BACK_LEFT_MOTOR 13
-#define BACK_RIGHT_MOTOR 20
-#define FRONT_INTAKE_MOTOR 5
-#define LEFT_LIFT_MOTOR 11
-#define RIGHT_LIFT_MOTOR 1
-#define BACK_CLAW 15
 
 using namespace okapi;
 
-okapi::Motor fL(FRONT_LEFT_MOTOR, false, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
-okapi::Motor fR(FRONT_RIGHT_MOTOR, true, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
-okapi::Motor bL(BACK_LEFT_MOTOR, false, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
-okapi::Motor bR(BACK_RIGHT_MOTOR, true, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
-okapi::Motor intake(FRONT_INTAKE_MOTOR, true, AbstractMotor::gearset::red, AbstractMotor::encoderUnits::degrees);
-okapi::Motor liftL(LEFT_LIFT_MOTOR, true, AbstractMotor::gearset::red, AbstractMotor::encoderUnits::degrees);
-okapi::Motor liftR(RIGHT_LIFT_MOTOR, false, AbstractMotor::gearset::red, AbstractMotor::encoderUnits::degrees);
-okapi::Motor claw(BACK_CLAW, false, AbstractMotor::gearset::red, AbstractMotor::encoderUnits::degrees);
+okapi::Motor drive_fL(FRONT_LEFT_MOTOR, false, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
+okapi::Motor drive_fR(FRONT_RIGHT_MOTOR, true, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
+okapi::Motor drive_bL(BACK_LEFT_MOTOR, false, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
+okapi::Motor drive_bR(BACK_RIGHT_MOTOR, true, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
+okapi::Motor front_intake(FRONT_INTAKE_MOTOR, true, AbstractMotor::gearset::red, AbstractMotor::encoderUnits::degrees);
+okapi::Motor back_intake(BACK_INTAKE_MOTOR, false, AbstractMotor::gearset::red, AbstractMotor::encoderUnits::degrees);
+okapi::Motor lift(LIFT_MOTOR, true, AbstractMotor::gearset::green, AbstractMotor::encoderUnits::degrees);
+
 std::shared_ptr<okapi::ChassisController> chassis = ChassisControllerBuilder()
     .withMotors(
-        {fL, bL},
-        {fR, bR}
+        {drive_fL, drive_bL},
+        {drive_fR, drive_bR}
     ).withDimensions(
         AbstractMotor::gearset::green,
         {
             {4.0_in, 12.0_in},
             static_cast<int32_t>(imev5GreenTPR)
         }
+    ).withMaxVelocity(180
     ).build();
 
 okapi::Controller joystick;
-okapi::ControllerButton takeIn(ControllerDigital::L1);
-okapi::ControllerButton takeOut(ControllerDigital::R1);
+okapi::ControllerButton backTakeIn(ControllerDigital::L1);
+okapi::ControllerButton backTakeOut(ControllerDigital::R1);
 okapi::ControllerButton liftUp(ControllerDigital::up);
 okapi::ControllerButton liftDown(ControllerDigital::down);
-okapi::ControllerButton clawOpen(ControllerDigital::left);
-okapi::ControllerButton clawClose(ControllerDigital::right);
+okapi::ControllerButton frontTakeIn(ControllerDigital::L2);
+okapi::ControllerButton frontTakeOut(ControllerDigital::R2);
 
-okapi::MotorGroup lift({liftL, liftR});
-okapi::ADIUltrasonic ultraRight('C', 'B'); //output - B, input - C
-okapi::Potentiometer pot('D');
-okapi::ADIButton intakeLimitSwitch('E');
-okapi::ADIUltrasonic ultraLeft('G', 'F'); //output - F, input - G
-okapi::ADIButton liftLimitSwitch('H');
+okapi::ADIButton back_bumperswitch('H');
+okapi::ADIGyro gyro('B');
