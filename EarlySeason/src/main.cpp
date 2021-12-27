9dbdda4a42860627d0dbe649a63fd81d0a26a320
Samyutha Srinivasan <65869364+samyuthasrinivasan@users.noreply.github.com>
2021-12-27 14:49:41 -0500

    Update main.cpp

--- EarlySeason/src/main.cpp
+++ EarlySeason/src/main.cpp
@@ -1,7 +1,6 @@
 #include "main.h"
 #include "config.h"
 using namespace okapi;
-
 void initialize() {
   front_intake.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
   back_intake.setBrakeMode(okapi::AbstractMotor::brakeMode::hold);
@@ -10,10 +9,15 @@ void initialize() {
   back_intake.tarePosition();
   lift.tarePosition();
   auton_selector();
-  gyro.reset();
 }
 
 void autonomous() {
+  printf("in autonomous");
+  joystick.clear();
+  delay(100);
+  joystick.setText(0, 0, "LOL");
+  delay(100);
+  printf("%d\n", i);
   switch(i){
     case 0: mbl_goal();
     break;
@@ -42,7 +46,7 @@ void opcontrol() {
       front_intake.moveVoltage(0);
       delay(10);
     }
-    
+
     if(backTakeIn.isPressed()){
       back_clamp();
     }
@@ -63,6 +67,6 @@ void opcontrol() {
       lift.moveVelocity(0);
     }
 
-    delay(10);
+    delay(20);
   }
 }
